be28cc6264f1cb99482702687b2f1563a78ee046
admin <admin@teju2629>
2022-02-09 17:34:33 +0530

    Day one proj

---
+++ Proj one/src/Java.java
@@ -0,0 +1,26 @@
+
+
+
+public class Java{
+	int nos;
+	float hrs;
+	int noc;
+	void displayStudents() {//m1
+		System.out.println("Number of Students");
+	}
+	void displayHours() {//m2
+		System.out.println("Number of Hours");
+	}
+	void displayClass() {//m3
+		System.out.println("Number of Class");
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 Java am= new Java();
+		am.displayStudents();
+	}
+
+}
+
