0cfc2341ec7d784195052c1ee71977b07bae5da6
admin <tejashwini2612@gmail.com>
2022-02-27 21:11:09 +0530

    Enum

---
+++ Enumerators/src/Enumset.java
@@ -0,0 +1,12 @@
+public class Enumset {
+  enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+  }
+
+  public static void main(String[] args) {
+    Level myVar = Level.MEDIUM; 
+    System.out.println(myVar);
+  }
+}
